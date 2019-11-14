19da020427a5db557baef1c99e3544bd185fdf9a
Tomanator <awesomedudegod1st@gmail.com>
2019-11-14 15:06:54 +1100

    added a bunch of text code for the menu (riley will edit later). LT

--- js/menu.js
+++ js/menu.js
@@ -8,6 +8,26 @@ var menuState = {
       //  if(space.isDown) {
         //game.state.start('level');
         space = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+
+          //text
+        game.add.sprite(150, 0,'bg1');
+
+        title = game.add.text(game.world.centerX, game.world.centerY-100, 'Mage Defense', {font: '50px Arial', fill: '#C70039'});
+        title.anchor.setTo(0.5, 0.5);
+    
+        help = game.add.text(80, game.world.centerY+50, '', {font: '25px Arial', fill: '#ffffff'});
+        helpText = 'Use the mouse to aim the mage towers and \n';
+        helpText += 'protect the kingdom from the meteors';
+        help.text = helpText;
+
+        instructions = game.add.text(80, game.world.height-80, 'Hit "BEGIN" if you think you are ready', {font: '25px Arial', fill: '#ffffff'});
+
+        button = game.add.button(game.world.centerX, game.world.centerY, 'button');
+        button.anchor.setTo(0.5,0.5);
+        button.onInputUp.add(this.start);
+        text = game.add.text(button.x,button.y,'BEGIN');
+        text.anchor.setTo(0.5,0.5);
+
       }
 
    ,
